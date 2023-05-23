f41113387de2b56e517e5d035637ac3991edc19d
Ye Tun <yetun71098@gmail.com>
2023-05-23 16:34:09 +0630

    Push

--- odoo-training/training_center/models/course_enrollment.py
+++ odoo-training/training_center/models/course_enrollment.py
@@ -4,6 +4,7 @@ class CourseEnrollment(models.Model):
     _name="training_center.course_enrollment"
     _description="Course Enrollment"
 
-    title=fields.Char("Title")
-    currency_id = fields.Many2one("res.currency")
-    price =fields.Monetary("Price","currency_id")
+    title=fields.Char("Title",required=True)
+    # currency_id = fields.Many2one("res.currency")
+    # price =fields.Monetary("Price","currency_id")
+    start_date=fields.Date(default=lambda self: fields.Date.today())
